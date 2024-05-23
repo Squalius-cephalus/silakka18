e81d0b2078eee405700b8cb0075a6463f170de7a
Squalius-cephalus <juho.taskinen@edu.turkuamk.fi>
2024-05-23 19:39:04 +0300

    Initial commit

---
+++ firmware/silakka18/keymaps/default/keymap.c
@@ -0,0 +1,35 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ENT│
+     * ├───┼───┼───┤───┤
+     * │ 4 │ 5 │ 6 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ + │
+     * ├───┼───┼───┤───┤
+     * │ / │ 0 │ . │SFT│
+     * └───┴───┴───┴───┘
+     * ┌────┐ ┌────┐
+     * │MUTE│ │STOP│
+     * └────┘ └────┘
+     */
+    [0] = LAYOUT_numpad_4x4_2(
+        KC_P7,   KC_P8,   KC_P9,   KC_KP_ENTER,
+        KC_P4,   KC_P5,   KC_P6,   KC_KP_MINUS,
+        KC_P1,   KC_P2,   KC_P3,   KC_KP_PLUS,
+        KC_PSLS,   KC_P0,   KC_KP_DOT,   KC_LSFT,
+        KC_AUDIO_MUTE,   KC_MPLY
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK)  },
+
+};
+#endif
---
+++ firmware/silakka18/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0xC7, 0x8F, 0x7C, 0x31, 0x3E, 0xFB, 0x37, 0x3E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 1 }
